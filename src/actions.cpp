ummonai/forgottenserver

commit c1b31eca3a4df55ce6180bfb1ea5de177428ca65

[Revscriptsys] fixing memory leak in Actions & Moveevents (#4742)
Moved all std::vector out of the pointer class itself and put them into their singleton classes (Actions/MoveEvents)
They're now stored in a std::map map by their pointer as a std::vector and once it is registered it clears the std::map with the pointer index.

src/actions.cpp
@@ -191,9 +191,9 @@ bool Actions::registerEvent(Event_ptr event, const pugi::xml_node& node)
 bool Actions::registerLuaEvent(Action* event)
 {
 	Action_ptr action{event};
-	if (!action->getItemIdRange().empty()) {
-		const auto& range = action->getItemIdRange();
-		for (auto id : range) {
+	if (!getItemIdRange(event).empty()) {
+		const auto& range = getItemIdRange(event);
+		for (auto& id : range) {
 			auto result = useItemMap.emplace(id, *action);
 			if (!result.second) {
 				std::cout << "[Warning - Actions::registerLuaEvent] Duplicate registered item with id: " << id
@@ -201,9 +201,9 @@ bool Actions::registerLuaEvent(Action* event)
 			}
 		}
 		return true;
-	} else if (!action->getUniqueIdRange().empty()) {
-		const auto& range = action->getUniqueIdRange();
-		for (auto id : range) {
+	} else if (!getUniqueIdRange(event).empty()) {
+		const auto& range = getUniqueIdRange(event);
+		for (auto& id : range) {
 			auto result = uniqueItemMap.emplace(id, *action);
 			if (!result.second) {
 				std::cout << "[Warning - Actions::registerLuaEvent] Duplicate registered item with uid: " << id
@@ -211,9 +211,9 @@ bool Actions::registerLuaEvent(Action* event)
 			}
 		}
 		return true;
-	} else if (!action->getActionIdRange().empty()) {
-		const auto& range = action->getActionIdRange();
-		for (auto id : range) {
+	} else if (!getActionIdRange(event).empty()) {
+		const auto& range = getActionIdRange(event);
+		for (auto& id : range) {
 			auto result = actionItemMap.emplace(id, *action);
 			if (!result.second) {
 				std::cout << "[Warning - Actions::registerLuaEvent] Duplicate registered item with aid: " << id

src/actions.h
@@ -34,18 +34,6 @@ public:
 	bool getCheckFloor() const { return checkFloor; }
 	void setCheckFloor(bool v) { checkFloor = v; }
 
-	void clearItemIdRange() { return ids.clear(); }
-	const std::vector<uint16_t>& getItemIdRange() const { return ids; }
-	void addItemId(uint16_t id) { ids.emplace_back(id); }
-
-	void clearUniqueIdRange() { return uids.clear(); }
-	const std::vector<uint16_t>& getUniqueIdRange() const { return uids; }
-	void addUniqueId(uint16_t id) { uids.emplace_back(id); }
-
-	void clearActionIdRange() { return aids.clear(); }
-	const std::vector<uint16_t>& getActionIdRange() const { return aids; }
-	void addActionId(uint16_t id) { aids.emplace_back(id); }
-
 	virtual ReturnValue canExecuteAction(const Player* player, const Position& toPos);
 	virtual bool hasOwnErrorHandler() { return false; }
 	virtual Thing* getTarget(Player* player, Creature* targetCreature, const Position& toPosition,
@@ -59,9 +47,6 @@ private:
 	bool allowFarUse = false;
 	bool checkFloor = true;
 	bool checkLineOfSight = true;
-	std::vector<uint16_t> ids;
-	std::vector<uint16_t> uids;
-	std::vector<uint16_t> aids;
 };
 
 class Actions final : public BaseEvents
@@ -85,6 +70,18 @@ public:
 	bool registerLuaEvent(Action* event);
 	void clear(bool fromLua) override final;
 
+	void clearItemIdRange(Action* action) { ids.erase(action); }
+	const std::vector<uint16_t>& getItemIdRange(Action* action) const { return ids.at(action); }
+	void addItemId(Action* action, uint16_t id) { ids[action].emplace_back(id); }
+
+	void clearUniqueIdRange(Action* action) { uids.erase(action); }
+	const std::vector<uint16_t>& getUniqueIdRange(Action* action) const { return uids.at(action); }
+	void addUniqueId(Action* action, uint16_t id) { uids[action].emplace_back(id); }
+
+	void clearActionIdRange(Action* action) { aids.erase(action); }
+	const std::vector<uint16_t>& getActionIdRange(Action* action) const { return aids.at(action); }
+	void addActionId(Action* action, uint16_t id) { aids[action].emplace_back(id); }
+
 private:
 	ReturnValue internalUseItem(Player* player, const Position& pos, uint8_t index, Item* item, bool isHotkey);
 
@@ -97,6 +94,9 @@ private:
 	ActionUseMap useItemMap;
 	ActionUseMap uniqueItemMap;
 	ActionUseMap actionItemMap;
+	std::map<Action*, std::vector<uint16_t>> ids;
+	std::map<Action*, std::vector<uint16_t>> uids;
+	std::map<Action*, std::vector<uint16_t>> aids;
 
 	Action* getAction(const Item* item);
 	void clearMap(ActionUseMap& map, bool fromLua);

src/luascript.cpp
@@ -17507,9 +17507,9 @@ int LuaScriptInterface::luaActionRegister(lua_State* L)
 			return 1;
 		}
 		tfs::lua::pushBoolean(L, g_actions->registerLuaEvent(action));
-		action->clearActionIdRange();
-		action->clearItemIdRange();
-		action->clearUniqueIdRange();
+		g_actions->clearItemIdRange(action);
+		g_actions->clearUniqueIdRange(action);
+		g_actions->clearActionIdRange(action);
 	} else {
 		lua_pushnil(L);
 	}
@@ -17524,10 +17524,10 @@ int LuaScriptInterface::luaActionItemId(lua_State* L)
 		int parameters = lua_gettop(L) - 1; // - 1 because self is a parameter aswell, which we want to skip ofc
 		if (parameters > 1) {
 			for (int i = 0; i < parameters; ++i) {
-				action->addItemId(tfs::lua::getNumber<uint32_t>(L, 2 + i));
+				g_actions->addItemId(action, tfs::lua::getNumber<uint32_t>(L, 2 + i));
 			}
 		} else {
-			action->addItemId(tfs::lua::getNumber<uint32_t>(L, 2));
+			g_actions->addItemId(action, tfs::lua::getNumber<uint32_t>(L, 2));
 		}
 		tfs::lua::pushBoolean(L, true);
 	} else {
@@ -17544,10 +17544,10 @@ int LuaScriptInterface::luaActionActionId(lua_State* L)
 		int parameters = lua_gettop(L) - 1; // - 1 because self is a parameter aswell, which we want to skip ofc
 		if (parameters > 1) {
 			for (int i = 0; i < parameters; ++i) {
-				action->addActionId(tfs::lua::getNumber<uint32_t>(L, 2 + i));
+				g_actions->addActionId(action, tfs::lua::getNumber<uint32_t>(L, 2 + i));
 			}
 		} else {
-			action->addActionId(tfs::lua::getNumber<uint32_t>(L, 2));
+			g_actions->addActionId(action, tfs::lua::getNumber<uint32_t>(L, 2));
 		}
 		tfs::lua::pushBoolean(L, true);
 	} else {
@@ -17564,10 +17564,10 @@ int LuaScriptInterface::luaActionUniqueId(lua_State* L)
 		int parameters = lua_gettop(L) - 1; // - 1 because self is a parameter aswell, which we want to skip ofc
 		if (parameters > 1) {
 			for (int i = 0; i < parameters; ++i) {
-				action->addUniqueId(tfs::lua::getNumber<uint32_t>(L, 2 + i));
+				g_actions->addUniqueId(action, tfs::lua::getNumber<uint32_t>(L, 2 + i));
 			}
 		} else {
-			action->addUniqueId(tfs::lua::getNumber<uint32_t>(L, 2));
+			g_actions->addUniqueId(action, tfs::lua::getNumber<uint32_t>(L, 2));
 		}
 		tfs::lua::pushBoolean(L, true);
 	} else {
@@ -17857,19 +17857,20 @@ int LuaScriptInterface::luaMoveEventRegister(lua_State* L)
 	if (moveevent) {
 		if ((moveevent->getEventType() == MOVE_EVENT_EQUIP || moveevent->getEventType() == MOVE_EVENT_DEEQUIP) &&
 		    moveevent->getSlot() == SLOTP_WHEREEVER) {
-			uint32_t id = moveevent->getItemIdRange().at(0);
+			uint32_t id = g_moveEvents->getItemIdRange(moveevent).at(0);
 			ItemType& it = Item::items.getItemType(id);
 			moveevent->setSlot(it.slotPosition);
 		}
 		if (!moveevent->isScripted()) {
 			tfs::lua::pushBoolean(L, g_moveEvents->registerLuaFunction(moveevent));
+			g_moveEvents->clearItemIdRange(moveevent);
 			return 1;
 		}
 		tfs::lua::pushBoolean(L, g_moveEvents->registerLuaEvent(moveevent));
-		moveevent->clearItemIdRange();
-		moveevent->clearActionIdRange();
-		moveevent->clearUniqueIdRange();
-		moveevent->clearPosList();
+		g_moveEvents->clearItemIdRange(moveevent);
+		g_moveEvents->clearActionIdRange(moveevent);
+		g_moveEvents->clearUniqueIdRange(moveevent);
+		g_moveEvents->clearPosList(moveevent);
 	} else {
 		lua_pushnil(L);
 	}
@@ -18039,10 +18040,10 @@ int LuaScriptInterface::luaMoveEventItemId(lua_State* L)
 		int parameters = lua_gettop(L) - 1; // - 1 because self is a parameter aswell, which we want to skip ofc
 		if (parameters > 1) {
 			for (int i = 0; i < parameters; ++i) {
-				moveevent->addItemId(tfs::lua::getNumber<uint32_t>(L, 2 + i));
+				g_moveEvents->addItemId(moveevent, tfs::lua::getNumber<uint32_t>(L, 2 + i));
 			}
 		} else {
-			moveevent->addItemId(tfs::lua::getNumber<uint32_t>(L, 2));
+			g_moveEvents->addItemId(moveevent, tfs::lua::getNumber<uint32_t>(L, 2));
 		}
 		tfs::lua::pushBoolean(L, true);
 	} else {
@@ -18059,10 +18060,10 @@ int LuaScriptInterface::luaMoveEventActionId(lua_State* L)
 		int parameters = lua_gettop(L) - 1; // - 1 because self is a parameter aswell, which we want to skip ofc
 		if (parameters > 1) {
 			for (int i = 0; i < parameters; ++i) {
-				moveevent->addActionId(tfs::lua::getNumber<uint32_t>(L, 2 + i));
+				g_moveEvents->addActionId(moveevent, tfs::lua::getNumber<uint32_t>(L, 2 + i));
 			}
 		} else {
-			moveevent->addActionId(tfs::lua::getNumber<uint32_t>(L, 2));
+			g_moveEvents->addActionId(moveevent, tfs::lua::getNumber<uint32_t>(L, 2));
 		}
 		tfs::lua::pushBoolean(L, true);
 	} else {
@@ -18079,10 +18080,10 @@ int LuaScriptInterface::luaMoveEventUniqueId(lua_State* L)
 		int parameters = lua_gettop(L) - 1; // - 1 because self is a parameter aswell, which we want to skip ofc
 		if (parameters > 1) {
 			for (int i = 0; i < parameters; ++i) {
-				moveevent->addUniqueId(tfs::lua::getNumber<uint32_t>(L, 2 + i));
+				g_moveEvents->addUniqueId(moveevent, tfs::lua::getNumber<uint32_t>(L, 2 + i));
 			}
 		} else {
-			moveevent->addUniqueId(tfs::lua::getNumber<uint32_t>(L, 2));
+			g_moveEvents->addUniqueId(moveevent, tfs::lua::getNumber<uint32_t>(L, 2));
 		}
 		tfs::lua::pushBoolean(L, true);
 	} else {
@@ -18099,10 +18100,10 @@ int LuaScriptInterface::luaMoveEventPosition(lua_State* L)
 		int parameters = lua_gettop(L) - 1; // - 1 because self is a parameter aswell, which we want to skip ofc
 		if (parameters > 1) {
 			for (int i = 0; i < parameters; ++i) {
-				moveevent->addPosList(tfs::lua::getPosition(L, 2 + i));
+				g_moveEvents->addPosList(moveevent, tfs::lua::getPosition(L, 2 + i));
 			}
 		} else {
-			moveevent->addPosList(tfs::lua::getPosition(L, 2));
+			g_moveEvents->addPosList(moveevent, tfs::lua::getPosition(L, 2));
 		}
 		tfs::lua::pushBoolean(L, true);
 	} else {

src/movement.cpp
@@ -190,10 +190,9 @@ bool MoveEvents::registerLuaFunction(MoveEvent* event)
 		}
 	}
 
-	if (moveEvent->getItemIdRange().size() > 0) {
-		if (moveEvent->getItemIdRange().size() == 1) {
-			uint32_t id = moveEvent->getItemIdRange().at(0);
-			addEvent(*moveEvent, id, itemIdMap);
+	if (!getItemIdRange(event).empty()) {
+		const auto& range = getItemIdRange(event);
+		for (auto& id : range) {
 			if (moveEvent->getEventType() == MOVE_EVENT_EQUIP) {
 				ItemType& it = Item::items.getItemType(id);
 				it.wieldInfo = moveEvent->getWieldInfo();
@@ -201,18 +200,7 @@ bool MoveEvents::registerLuaFunction(MoveEvent* event)
 				it.minReqMagicLevel = moveEvent->getReqMagLv();
 				it.vocationString = moveEvent->getVocationString();
 			}
-		} else {
-			uint32_t iterId = 0;
-			while (++iterId < moveEvent->getItemIdRange().size()) {
-				if (moveEvent->getEventType() == MOVE_EVENT_EQUIP) {
-					ItemType& it = Item::items.getItemType(moveEvent->getItemIdRange().at(iterId));
-					it.wieldInfo = moveEvent->getWieldInfo();
-					it.minReqLevel = moveEvent->getReqLevel();
-					it.minReqMagicLevel = moveEvent->getReqMagLv();
-					it.vocationString = moveEvent->getVocationString();
-				}
-				addEvent(*moveEvent, moveEvent->getItemIdRange().at(iterId), itemIdMap);
-			}
+			addEvent(*moveEvent, id, itemIdMap);
 		}
 	} else {
 		return false;
@@ -240,10 +228,9 @@ bool MoveEvents::registerLuaEvent(MoveEvent* event)
 		}
 	}
 
-	if (moveEvent->getItemIdRange().size() > 0) {
-		if (moveEvent->getItemIdRange().size() == 1) {
-			uint32_t id = moveEvent->getItemIdRange().at(0);
-			addEvent(*moveEvent, id, itemIdMap);
+	if (!getItemIdRange(event).empty()) {
+		const auto& range = getItemIdRange(event);
+		for (auto& id : range) {
 			if (moveEvent->getEventType() == MOVE_EVENT_EQUIP) {
 				ItemType& it = Item::items.getItemType(id);
 				it.wieldInfo = moveEvent->getWieldInfo();
@@ -251,53 +238,26 @@ bool MoveEvents::registerLuaEvent(MoveEvent* event)
 				it.minReqMagicLevel = moveEvent->getReqMagLv();
 				it.vocationString = moveEvent->getVocationString();
 			}
-		} else {
-			auto v = moveEvent->getItemIdRange();
-			for (auto i = v.begin(); i != v.end(); i++) {
-				if (moveEvent->getEventType() == MOVE_EVENT_EQUIP) {
-					ItemType& it = Item::items.getItemType(*i);
-					it.wieldInfo = moveEvent->getWieldInfo();
-					it.minReqLevel = moveEvent->getReqLevel();
-					it.minReqMagicLevel = moveEvent->getReqMagLv();
-					it.vocationString = moveEvent->getVocationString();
-				}
-				addEvent(*moveEvent, *i, itemIdMap);
-			}
+			addEvent(*moveEvent, id, itemIdMap);
 		}
-	} else if (moveEvent->getActionIdRange().size() > 0) {
-		if (moveEvent->getActionIdRange().size() == 1) {
-			int32_t id = moveEvent->getActionIdRange().at(0);
+	} else if (!getActionIdRange(event).empty()) {
+		const auto& range = getActionIdRange(event);
+		for (auto& id : range) {
 			addEvent(*moveEvent, id, actionIdMap);
-		} else {
-			auto v = moveEvent->getActionIdRange();
-			for (auto i = v.begin(); i != v.end(); i++) {
-				addEvent(*moveEvent, *i, actionIdMap);
-			}
 		}
-	} else if (moveEvent->getUniqueIdRange().size() > 0) {
-		if (moveEvent->getUniqueIdRange().size() == 1) {
-			int32_t id = moveEvent->getUniqueIdRange().at(0);
+	} else if (!getUniqueIdRange(event).empty()) {
+		const auto& range = getUniqueIdRange(event);
+		for (auto& id : range) {
 			addEvent(*moveEvent, id, uniqueIdMap);
-		} else {
-			auto v = moveEvent->getUniqueIdRange();
-			for (auto i = v.begin(); i != v.end(); i++) {
-				addEvent(*moveEvent, *i, uniqueIdMap);
-			}
 		}
-	} else if (moveEvent->getPosList().size() > 0) {
-		if (moveEvent->getPosList().size() == 1) {
-			Position pos = moveEvent->getPosList().at(0);
+	} else if (!getPosList(event).empty()) {
+		const auto& range = getPosList(event);
+		for (auto& pos : range) {
 			addEvent(*moveEvent, pos, positionMap);
-		} else {
-			auto v = moveEvent->getPosList();
-			for (auto i = v.begin(); i != v.end(); i++) {
-				addEvent(*moveEvent, *i, positionMap);
-			}
 		}
 	} else {
 		return false;
 	}
-
 	return true;
 }
 

src/movement.h
@@ -56,6 +56,22 @@ public:
 	bool registerLuaFunction(MoveEvent* event);
 	void clear(bool fromLua) override final;
 
+	void clearItemIdRange(MoveEvent* event) { itemIdRange.erase(event); }
+	const std::vector<uint32_t>& getItemIdRange(MoveEvent* event) const { return itemIdRange.at(event); }
+	void addItemId(MoveEvent* event, uint32_t id) { itemIdRange[event].emplace_back(id); }
+
+	void clearActionIdRange(MoveEvent* event) { actionIdRange.erase(event); }
+	const std::vector<uint32_t>& getActionIdRange(MoveEvent* event) const { return actionIdRange.at(event); }
+	void addActionId(MoveEvent* event, uint32_t id) { actionIdRange[event].emplace_back(id); }
+
+	void clearUniqueIdRange(MoveEvent* event) { uniqueIdRange.erase(event); }
+	const std::vector<uint32_t>& getUniqueIdRange(MoveEvent* event) const { return uniqueIdRange.at(event); }
+	void addUniqueId(MoveEvent* event, uint32_t id) { uniqueIdRange[event].emplace_back(id); }
+
+	void clearPosList(MoveEvent* event) { posList.erase(event); }
+	const std::vector<Position>& getPosList(MoveEvent* event) const { return posList.at(event); }
+	void addPosList(MoveEvent* event, Position pos) { posList[event].emplace_back(pos); }
+
 private:
 	using MoveListMap = std::map<int32_t, MoveEventList>;
 	using MovePosListMap = std::map<Position, MoveEventList>;
@@ -78,6 +94,10 @@ private:
 	MoveListMap actionIdMap;
 	MoveListMap itemIdMap;
 	MovePosListMap positionMap;
+	std::map<MoveEvent*, std::vector<uint32_t>> itemIdRange;
+	std::map<MoveEvent*, std::vector<uint32_t>> actionIdRange;
+	std::map<MoveEvent*, std::vector<uint32_t>> uniqueIdRange;
+	std::map<MoveEvent*, std::vector<Position>> posList;
 
 	LuaScriptInterface scriptInterface;
 };
@@ -132,18 +152,6 @@ public:
 	}
 	bool getTileItem() const { return tileItem; }
 	void setTileItem(bool b) { tileItem = b; }
-	void clearItemIdRange() { return itemIdRange.clear(); }
-	const std::vector<uint32_t>& getItemIdRange() const { return itemIdRange; }
-	void addItemId(uint32_t id) { itemIdRange.emplace_back(id); }
-	void clearActionIdRange() { return actionIdRange.clear(); }
-	const std::vector<uint32_t>& getActionIdRange() const { return actionIdRange; }
-	void addActionId(uint32_t id) { actionIdRange.emplace_back(id); }
-	void clearUniqueIdRange() { return uniqueIdRange.clear(); }
-	const std::vector<uint32_t>& getUniqueIdRange() const { return uniqueIdRange; }
-	void addUniqueId(uint32_t id) { uniqueIdRange.emplace_back(id); }
-	void clearPosList() { return posList.clear(); }
-	const std::vector<Position>& getPosList() const { return posList; }
-	void addPosList(Position pos) { posList.emplace_back(pos); }
 	void setSlot(uint32_t s) { slot = s; }
 	uint32_t getRequiredLevel() { return reqLevel; }
 	void setRequiredLevel(uint32_t level) { reqLevel = level; }
@@ -181,11 +189,6 @@ private:
 	uint32_t wieldInfo = 0;
 	std::unordered_set<uint16_t> vocationEquipSet;
 	bool tileItem = false;
-
-	std::vector<uint32_t> itemIdRange;
-	std::vector<uint32_t> actionIdRange;
-	std::vector<uint32_t> uniqueIdRange;
-	std::vector<Position> posList;
 };
 
 #endif // FS_MOVEMENT_H

commit 2366f98014e1fd2cde7c3f5c3807c2ead7939a72

[Revscriptsys] fix crash introduced in #4742 (#4750)
- checking first if the pointer has a map (is valid) before trying to get it's content

src/actions.cpp
@@ -191,7 +191,7 @@ bool Actions::registerEvent(Event_ptr event, const pugi::xml_node& node)
 bool Actions::registerLuaEvent(Action* event)
 {
 	Action_ptr action{event};
-	if (!getItemIdRange(event).empty()) {
+	if (isValid(ids, event)) {
 		const auto& range = getItemIdRange(event);
 		for (auto& id : range) {
 			auto result = useItemMap.emplace(id, *action);
@@ -201,7 +201,7 @@ bool Actions::registerLuaEvent(Action* event)
 			}
 		}
 		return true;
-	} else if (!getUniqueIdRange(event).empty()) {
+	} else if (isValid(uids, event)) {
 		const auto& range = getUniqueIdRange(event);
 		for (auto& id : range) {
 			auto result = uniqueItemMap.emplace(id, *action);
@@ -211,7 +211,7 @@ bool Actions::registerLuaEvent(Action* event)
 			}
 		}
 		return true;
-	} else if (!getActionIdRange(event).empty()) {
+	} else if (isValid(aids, event)) {
 		const auto& range = getActionIdRange(event);
 		for (auto& id : range) {
 			auto result = actionItemMap.emplace(id, *action);

src/actions.h
@@ -70,6 +70,7 @@ public:
 	bool registerLuaEvent(Action* event);
 	void clear(bool fromLua) override final;
 
+	bool isValid(std::map<Action*, std::vector<uint16_t>> map, Action* action) { return map.find(action) != map.end(); }
 	void clearItemIdRange(Action* action) { ids.erase(action); }
 	const std::vector<uint16_t>& getItemIdRange(Action* action) const { return ids.at(action); }
 	void addItemId(Action* action, uint16_t id) { ids[action].emplace_back(id); }

src/luascript.cpp
@@ -17524,10 +17524,10 @@ int LuaScriptInterface::luaActionItemId(lua_State* L)
 		int parameters = lua_gettop(L) - 1; // - 1 because self is a parameter aswell, which we want to skip ofc
 		if (parameters > 1) {
 			for (int i = 0; i < parameters; ++i) {
-				g_actions->addItemId(action, tfs::lua::getNumber<uint32_t>(L, 2 + i));
+				g_actions->addItemId(action, tfs::lua::getNumber<uint16_t>(L, 2 + i));
 			}
 		} else {
-			g_actions->addItemId(action, tfs::lua::getNumber<uint32_t>(L, 2));
+			g_actions->addItemId(action, tfs::lua::getNumber<uint16_t>(L, 2));
 		}
 		tfs::lua::pushBoolean(L, true);
 	} else {
@@ -17544,10 +17544,10 @@ int LuaScriptInterface::luaActionActionId(lua_State* L)
 		int parameters = lua_gettop(L) - 1; // - 1 because self is a parameter aswell, which we want to skip ofc
 		if (parameters > 1) {
 			for (int i = 0; i < parameters; ++i) {
-				g_actions->addActionId(action, tfs::lua::getNumber<uint32_t>(L, 2 + i));
+				g_actions->addActionId(action, tfs::lua::getNumber<uint16_t>(L, 2 + i));
 			}
 		} else {
-			g_actions->addActionId(action, tfs::lua::getNumber<uint32_t>(L, 2));
+			g_actions->addActionId(action, tfs::lua::getNumber<uint16_t>(L, 2));
 		}
 		tfs::lua::pushBoolean(L, true);
 	} else {
@@ -17564,10 +17564,10 @@ int LuaScriptInterface::luaActionUniqueId(lua_State* L)
 		int parameters = lua_gettop(L) - 1; // - 1 because self is a parameter aswell, which we want to skip ofc
 		if (parameters > 1) {
 			for (int i = 0; i < parameters; ++i) {
-				g_actions->addUniqueId(action, tfs::lua::getNumber<uint32_t>(L, 2 + i));
+				g_actions->addUniqueId(action, tfs::lua::getNumber<uint16_t>(L, 2 + i));
 			}
 		} else {
-			g_actions->addUniqueId(action, tfs::lua::getNumber<uint32_t>(L, 2));
+			g_actions->addUniqueId(action, tfs::lua::getNumber<uint16_t>(L, 2));
 		}
 		tfs::lua::pushBoolean(L, true);
 	} else {

src/movement.cpp
@@ -190,7 +190,7 @@ bool MoveEvents::registerLuaFunction(MoveEvent* event)
 		}
 	}
 
-	if (!getItemIdRange(event).empty()) {
+	if (isValid(itemIdRange, event)) {
 		const auto& range = getItemIdRange(event);
 		for (auto& id : range) {
 			if (moveEvent->getEventType() == MOVE_EVENT_EQUIP) {
@@ -228,7 +228,7 @@ bool MoveEvents::registerLuaEvent(MoveEvent* event)
 		}
 	}
 
-	if (!getItemIdRange(event).empty()) {
+	if (isValid(itemIdRange, event)) {
 		const auto& range = getItemIdRange(event);
 		for (auto& id : range) {
 			if (moveEvent->getEventType() == MOVE_EVENT_EQUIP) {
@@ -240,17 +240,17 @@ bool MoveEvents::registerLuaEvent(MoveEvent* event)
 			}
 			addEvent(*moveEvent, id, itemIdMap);
 		}
-	} else if (!getActionIdRange(event).empty()) {
+	} else if (isValid(actionIdRange, event)) {
 		const auto& range = getActionIdRange(event);
 		for (auto& id : range) {
 			addEvent(*moveEvent, id, actionIdMap);
 		}
-	} else if (!getUniqueIdRange(event).empty()) {
+	} else if (isValid(uniqueIdRange, event)) {
 		const auto& range = getUniqueIdRange(event);
 		for (auto& id : range) {
 			addEvent(*moveEvent, id, uniqueIdMap);
 		}
-	} else if (!getPosList(event).empty()) {
+	} else if (isValidPos(posList, event)) {
 		const auto& range = getPosList(event);
 		for (auto& pos : range) {
 			addEvent(*moveEvent, pos, positionMap);

src/movement.h
@@ -56,6 +56,14 @@ public:
 	bool registerLuaFunction(MoveEvent* event);
 	void clear(bool fromLua) override final;
 
+	bool isValid(std::map<MoveEvent*, std::vector<uint32_t>> map, MoveEvent* event)
+	{
+		return map.find(event) != map.end();
+	}
+	bool isValidPos(std::map<MoveEvent*, std::vector<Position>> map, MoveEvent* event)
+	{
+		return map.find(event) != map.end();
+	}
 	void clearItemIdRange(MoveEvent* event) { itemIdRange.erase(event); }
 	const std::vector<uint32_t>& getItemIdRange(MoveEvent* event) const { return itemIdRange.at(event); }
 	void addItemId(MoveEvent* event, uint32_t id) { itemIdRange[event].emplace_back(id); }